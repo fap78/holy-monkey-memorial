ecd1b6a455149aa8dacaa53355a578e3b3a71631
CodeLord <91057422+fap78@users.noreply.github.com>
2022-02-24 15:31:58 +1100

    Add files via upload

---
+++ template.py
@@ -0,0 +1,179 @@
+"""-------------------- SECTION :: SETUP --------------------"""
+# (LIBRARIES)
+# system libraries
+import sys
+import sysconfig
+# end
+
+# ref
+#
+# end
+
+# (MODULES),
+# system time
+from time import sleep as sp
+# end
+
+# windows control [os]
+from os import system as st
+# end
+
+"""-------------------- SECTION :: FILES AND DATA -----------"""
+# (FILES : type <- referential)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+# (FILES : type <- data)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+# (VARIABLES)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+"""-------------------- SECTION :: DATA STRUCTURES ----------"""
+# (ARRAYS: type <- list)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+# (ARRAYS: type <- tuple)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+# (ARRAYS: type <- linked lists)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+# (DICTIONARIES)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+
+"""-------------------- SECTION :: CLASSES ------------------"""
+# (CLASS: type <- object)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+
+"""-------------------- SECTION :: FUNCTIONS ----------------"""
+# (FUNCTION: type <- no parameters)
+# clear terminal
+def clsT(e):
+    sp(e)
+    st('cls')
+# end
+
+# ref
+#
+# end
+
+# (FUNCTION: type <- parameters)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+
+"""-------------------- SECTION :: PROCESSES ----------------"""
+# (PROCESS: type <- !system)
+# clear terminal
+clsT(2)
+# end
+
+# ref
+#
+# end
+
+# (PROCESS: type <- !type)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+# (PROCESS: type <- !type)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+# (PROCESS: type <- !type)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+
+"""-------------------- SECTION :: UI -----------------------"""
+# (CLASS: type <- QMainWindow)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+
+"""-------------------- SECTION :: TESTS --------------------"""
+# (TEST: type <- !type)
+# ref
+#
+# end
+
+# ref
+#
+# end
+
+"""-------------------- ABORT! :: ALIENS ARE COMING-----------"""
